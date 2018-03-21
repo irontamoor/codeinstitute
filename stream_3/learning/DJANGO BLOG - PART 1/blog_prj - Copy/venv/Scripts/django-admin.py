f0482c4abc80bb587bb198c98899f2ff978187fd
irontamoor <tamoorahmed@ymail.com>
2018-03-21 13:40:00 +0000

    1

---
+++ stream_3/learning/DJANGO BLOG - PART 1/blog_prj - Copy/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\OneDrive\Learning Files\IT\Code Institute\Stream 3\DJANGO BLOG - PART 1\blog_prj\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ stream_3/learning/DJANGO BLOG - PART 1/blog_prjj - Copy/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\onedrive\learning files\it\code institute\stream 3\django blog - part 1\blog_prjj\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
